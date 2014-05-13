3d8d7d2f05012a6c3ebcebd8422c4f068106deab
Brynzak <ybrynzak@XIN850086.asv.local>
2014-05-13 11:50:37 +0200

    Fixed a bug with not specified file for upload

--- uploader.rb
+++ uploader.rb
@@ -5,28 +5,32 @@ require 'sinatra'
 require 'fileutils'
 require 'dir'
 
-  def img name
-    
-  end
+PATH = "./public/images"
         
-  def list_of_images(folder)
-    return Dir.entries(folder)
+def list_of_images(folder)
+  return Dir.entries(folder)
+end
+
+def list_images folder
+  list = list_of_images folder
+  return list.map { |item|  "<img src = 'images/#{item}' alt = '#{item}' height='100'/>" }
+end
+
+get '/' do
+  erb = ERB.new(File.read("./view/index.html.erb"))
+  erb.result(binding)
+end
+
+post '/' do
+
+  if !params[:file].nil?
+    tmpfile = params[:file][:tempfile]
+    filename = params[:file][:filename]
+
+    FileUtils.cp(tmpfile, "#{PATH}/#{filename}")
+  else
+    puts "Popup"
   end
 
-  def list_images folder
-    list = list_of_images folder
-    return list.map { |item|  "<img src = 'images/#{item}' alt = '#{item}' height='100'/>" }   
-  end
-
- get '/' do
-   erb = ERB.new(File.read("./view/index.html.erb"))
-   erb.result(binding)
- end
-
- post '/' do
-  PATH="./public/images"
-  tmpfile = params[:file][:tempfile]
-  filename = params[:file][:filename]
-  FileUtils.cp(tmpfile, "#{PATH}/#{filename}")
   redirect back
- end
+end
